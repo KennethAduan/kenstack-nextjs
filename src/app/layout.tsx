396231681c11f48f13978cfa4d11dd2f12333227
Kenneth <kenneth.aduan10@gmail.com>
2024-06-09 20:11:21 +0800

    update dark theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
+import MainAppProviders from "@/services/providers";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -27,7 +28,7 @@ export default function RootLayout({
           fontSans.variable
         )}
       >
-        {children}
+        <MainAppProviders>{children}</MainAppProviders>
       </body>
     </html>
   );
---
+++ src/services/providers/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ThemeProvider } from "./theme/theme-provider";
+
+const MainAppProviders = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default MainAppProviders;
